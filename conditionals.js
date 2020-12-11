84a83b2d0855a5e56b55f898f056ffee248ec9d6
Idiljei <i.jeilani@gmail.com>
2020-12-11 06:41:11 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -24,3 +24,24 @@ if (temperature < 0) {
 }
 
 console.log(temperature);
+
+// Using && operator 
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+// Using || or operator 
+
+if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea…");
+  }
+
+  // Using ! not operator 
+
+  if (!raining) {
+    console.log("Leave your umbrella at home!");
+  }
