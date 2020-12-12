3a2f46301bbd617fca328923aac71e87471b04c3
Idiljei <i.jeilani@gmail.com>
2020-12-12 05:42:46 +0000

    Testing some code

---
+++ tinkering.js
@@ -0,0 +1,33 @@
+function multiply(a, b) {
+    return a * b;
+  }
+  
+  const result = multiply(2, 4);
+  
+  console.log(result);
+
+  let name = 'hello friend';
+
+  const ASCII_value= name(0)
+
+  console.log (ASCII_value);
+
+  const x = null; 
+
+  console.log (x);
+
+  const thingOne= "red";
+  const thingTwo= "blue";
+
+  const weather = "sunny";
+
+  If (weather=== "snow") {
+      console.log ("wear a coat");
+    }  else if (weather==="rain") {
+        console.log ("wear a jacket");
+    } else {
+        console.log ("wear what you have on");
+    }
+
+  
+    
