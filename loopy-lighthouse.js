c41a20dcd85741fef517fa8cfe6696affb624e33
Idiljei <i.jeilani@gmail.com>
2020-12-13 03:40:30 +0000

    LoopyLighthouse Problem

---
+++ loopy-lighthouse.js
@@ -0,0 +1,17 @@
+
+
+
+let sound = "";
+let laugh = function (pitch) {
+
+  for ( let i =0; i < pitch; i++)  {
+    sound = sound + "ha" ; 
+
+  }
+   sound = sound + "!" ; 
+
+   return sound; 
+
+}
+
+console.log (laugh (2)); 
